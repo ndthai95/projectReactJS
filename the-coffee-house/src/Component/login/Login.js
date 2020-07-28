4bdcaec0d41a9a8ba23c400fb320889238f6390e
thaind2 <thaind2@smartosc.com>
2020-07-28 20:35:23 +0700

    login

--- the-coffee-house/src/Component/login/Login.js
+++ the-coffee-house/src/Component/login/Login.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import  './login.scss';
 
 class Login extends Component {
     render() {
         return (
             <div>
-                <h1 className="text-center">Login</h1>
+            <h1 className="text-center">Login</h1>
+            <div className="a">
+                    abc
+                    <div className="a__b">
+                        hello
+                    </div>
+                </div>
             </div>
         );
     }
