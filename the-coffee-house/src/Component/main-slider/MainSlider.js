e1c15d270970e62edc9ff0724e89c885484e6565
thanhtung2705 <thanhtung1996fsd@gmail.com>
2020-08-04 16:10:50 +0700

    create main slider

--- the-coffee-house/src/Component/main-slider/MainSlider.js
+++ the-coffee-house/src/Component/main-slider/MainSlider.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import './mainSlider.scss';
+import banner1 from './banner1.jpg';
+import banner2 from './banner2.jpg';
+import banner3 from './banner3.jpg';
 
 class MainSlider extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="main-slider">
+                <div className="main-slider__wrap">
+                    <div className="main-slider__image fade">
+                        <a href="#">
+                            <img src={banner1} alt="Banner1" />
+                        </a>
+                    </div>
+
+                    <div className="main-slider__image fade">
+                        <a href="#">
+                            <img src={banner2} alt="Banner2" />
+                        </a>
+                    </div>
+
+                    <div className="main-slider__image fade">
+                        <a href="#">
+                            <img src={banner3} alt="Banner3" />
+                        </a>
+                    </div>
+                </div>
+
+                <div class="dots">
+                    <div className="dots__wrap">
+                        <span className="dot" onclick="currentSlide(0)"></span>
+                        <span className="dot" onclick="currentSlide(1)"></span>
+                        <span className="dot" onclick="currentSlide(2)"></span>
+                    </div>
+                </div>
             </div>
         );
     }
